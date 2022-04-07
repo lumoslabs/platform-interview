56ef6cb4d1ece9a29021955e5f4700124d9063b3
Ryan Frantz <ryanleefrantz@gmail.com>
2022-04-07 14:46:42 -0400

    Adds small utils/defaults.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { app } from './app.js';
+import { defaults } from './utils.js';
+
+const host = process.env.HOST || defaults.host;
+const port = process.env.PORT || defaults.port;
 
 try {
-  await app.listen(3000);
+  await app.listen({ host: host, port: port });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+// A set of sane defaults.
+const defaults = {
+  host: '0.0.0.0',
+  port: 8080
+};
+
+export { defaults };
